8c7a63885c4031fe882a6239bbf4f7a921784c42
Wasim49 <102612450+Wasim49@users.noreply.github.com>
2024-12-12 11:35:27 +0000

    Add files via upload

---
+++ vault-setup-wrapper.ps1
@@ -0,0 +1,36 @@
+# Define the URLs of the scripts
+$installScriptUrl = "https://raw.githubusercontent.com/Wasim49/Github-Repo/refs/heads/main/vault-install.ps1"
+$checkScriptUrl = "https://raw.githubusercontent.com/Wasim49/Github-Repo/refs/heads/main/vault-manual-check.ps1"
+$storageScriptUrl = "https://raw.githubusercontent.com/Wasim49/Github-Repo/refs/heads/main/vault-storage-setup.ps1"
+
+# Define the local folder path where the scripts will be saved (vaultscripts folder inside Downloads)
+$downloadsFolder = "$env:USERPROFILE\Downloads\vaultscripts"
+
+# Define the local file paths where the scripts will be saved inside the vaultscripts folder
+$installScriptPath = "$downloadsFolder\vault-install.ps1"
+$checkScriptPath = "$downloadsFolder\vault-manual-check.ps1"
+$storageScriptPath = "$downloadsFolder\vault-storage-setup.ps1"
+
+# Create the vaultscripts folder inside Downloads if it doesn't exist
+if (-not (Test-Path -Path $downloadsFolder)) {
+    New-Item -Path $downloadsFolder -ItemType Directory
+}
+
+# Download the Vault installation script
+Invoke-WebRequest -Uri $installScriptUrl -OutFile $installScriptPath
+
+# Download the Vault manual check script
+Invoke-WebRequest -Uri $checkScriptUrl -OutFile $checkScriptPath
+
+# Download the Vault storage setup script
+Invoke-WebRequest -Uri $storageScriptUrl -OutFile $storageScriptPath
+
+# Execute the Vault installation script
+Start-Process powershell -ArgumentList "-ExecutionPolicy Unrestricted -File $installScriptPath" -Wait
+
+# Execute the Vault manual check script
+Start-Process powershell -ArgumentList "-ExecutionPolicy Unrestricted -File $checkScriptPath" -Wait
+
+# Execute the Vault storage setup script
+Start-Process powershell -ArgumentList "-ExecutionPolicy Unrestricted -File $storageScriptPath" -Wait
+
