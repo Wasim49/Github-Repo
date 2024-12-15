598bfc4a9347852ccf5d8cfda7b4aed361f4915b
Wasim49 <102612450+Wasim49@users.noreply.github.com>
2024-12-15 15:42:05 +0000

    Add files via upload

---
+++ openssh-wrapper.ps1
@@ -0,0 +1,36 @@
+# Define the path for the transcript log file
+$logFilePath = "$env:USERPROFILE\Downloads\opensshscripts\session.log"
+
+# Start the transcript to capture everything in the PowerShell session
+Start-Transcript -Path $logFilePath -Append
+
+Write-Host "Starting execution of OpenSSH installation script..."
+
+# Define the URL of the OpenSSH script
+$opensshScriptUrl = "https://raw.githubusercontent.com/Wasim49/Github-Repo/refs/heads/main/install-openssh.ps1"
+
+# Define the local folder path where the script will be saved (opensshscripts folder inside Downloads)
+$downloadsFolder = "$env:USERPROFILE\Downloads\opensshscripts"
+
+# Define the local file path where the OpenSSH script will be saved inside the opensshscripts folder
+$opensshScriptPath = "$downloadsFolder\install-openssh.ps1"
+
+# Create the opensshscripts folder inside Downloads if it doesn't exist
+if (-not (Test-Path -Path $downloadsFolder)) {
+    New-Item -Path $downloadsFolder -ItemType Directory
+    Write-Host "Created opensshscripts folder."
+}
+
+# Download the OpenSSH installation script
+Write-Host "Downloading OpenSSH Installation Script..."
+Invoke-WebRequest -Uri $opensshScriptUrl -OutFile $opensshScriptPath
+Write-Host "OpenSSH Installation Script downloaded to $opensshScriptPath."
+
+# Execute the OpenSSH installation script
+Write-Host "Executing OpenSSH Installation Script..."
+. $opensshScriptPath
+
+Write-Host "OpenSSH Installation script executed successfully."
+
+# Stop the transcript to end capturing the session
+Stop-Transcript
