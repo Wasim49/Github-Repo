ac4e41e6fcf2d960bf31056f9883cb286060d792
Wasim49 <wasim.navas-khan@capgemini.com>
2024-12-18 09:28:01 +0000

    added new scripts

--- wrapper-scripts/general-wrapper/general-wrapper.ps1
+++ wrapper-scripts/general-wrapper/general-wrapper.ps1
@@ -7,16 +7,16 @@ Start-Transcript -Path $logFilePath -Append
 Write-Host "Starting execution of wrapper script..."
 
 # Define the URLs of the scripts
-$installScriptUrl/installnotepad = "https://raw.githubusercontent.com/Wasim49/Github-Repo/refs/heads/main/vault-install.ps1"
-$checkScriptUrl/installvscode = "https://raw.githubusercontent.com/Wasim49/Github-Repo/refs/heads/main/vault-manual-check.ps1"
-$storageScriptUrl/can delete = "https://raw.githubusercontent.com/Wasim49/Github-Repo/refs/heads/main/vault-storage-setup.ps1"
+$notepadurl= "https://raw.githubusercontent.com/Wasim49/Github-Repo/refs/heads/main/actual-scripts/notepad/install.notepad.ps1"
+$vscodeurl = "https://raw.githubusercontent.com/Wasim49/Github-Repo/refs/heads/main/vault-manual-check.ps1"
+
 
 # Define the local folder path where the scripts will be saved (vaultscripts folder inside Downloads)
 $downloadsFolder = "$env:USERPROFILE\Downloads\scripts"
 
 # Define the local file paths where the scripts will be saved inside the vaultscripts folder
-$installScriptPath = "$downloadsFolder\vault-install.ps1"
-$checkScriptPath = "$downloadsFolder\vault-manual-check.ps1"
+$notepadpath = "$downloadsFolder\install-notepad.ps1"
+$vscodepath = "$downloadsFolder\install-vscode.ps1"
 
 # Create the vaultscripts folder inside Downloads if it doesn't exist
 if (-not (Test-Path -Path $downloadsFolder)) {
@@ -24,31 +24,31 @@ if (-not (Test-Path -Path $downloadsFolder)) {
     Write-Host "Created vaultscripts folder."
 }
 
-# Download the Vault installation script
-Write-Host "Downloading Vault Installation Script..."
-Invoke-WebRequest -Uri $installScriptUrl -OutFile $installScriptPath
-Write-Host "Vault Installation Script downloaded to $installScriptPath."
+# Download the notepad installation script
+Write-Host "Downloading notepad Installation Script..."
+Invoke-WebRequest -Uri $notepadurl -OutFile $notepadpath
+Write-Host "Vault Installation Script downloaded to $notepadpath."
 
-# Download the Vault manual check script
+# Download the vscode installation script
 Write-Host "Downloading Vault Manual Check Script..."
-Invoke-WebRequest -Uri $checkScriptUrl -OutFile $checkScriptPath
-Write-Host "Vault Manual Check Script downloaded to $checkScriptPath."
+Invoke-WebRequest -Uri $vscodeurl -OutFile $vscodepath
+Write-Host "Vault Manual Check Script downloaded to $vscodepath."
 
 
 # Execute the Vault installation script
 Write-Host "Executing Vault Installation Script..."
-. $installScriptPath
+. $notepadpath
 
 # Execute the Vault manual check script
 Write-Host "Executing Vault Manual Check Script..."
-. $checkScriptPath
+. $vscodepath
 
 
 Write-Host "All scripts executed successfully."
 
-Write-Host "Vault is ready. You can keep this master server session open or you can start master session in another powershell session using this command, vault server -config=""C:\Program Files\Vault"""
+Write-Host "Notepad is installed"
 
-Write-Host "Vault binary is at C:\ProgramData\chocolatey\bin\vault.exe. Secrets are persisted in C:\VaultData. Configuration file is at C:\Program Files\Vault. Downloads folder contains everything else"
+Write-Host "Vscode is installed"
 
 # Stop the transcript to end capturing the session
 Stop-Transcript
