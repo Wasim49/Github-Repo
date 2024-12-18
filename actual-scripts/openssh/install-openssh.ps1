c826ed7371d38fb7997ffab6bdb2ff525df516d0
Wasim49 <wasim.navas-khan@capgemini.com>
2024-12-18 13:55:43 +0000

    testing bastion folder

--- actual-scripts/openssh/install-openssh.ps1
+++ actual-scripts/openssh/install-openssh.ps1
@@ -1,36 +1,37 @@
 # Define the URL for the OpenSSH zip file and the installation path
-$zipUrl = "https://github.com/PowerShell/Win32-OpenSSH/releases/download/v9.8.1.0p1-Preview/OpenSSH-Win64.zip"
-$downloadPath = "$env:USERPROFILE\Downloads\OpenSSH-Win64.zip"
-$extractPath = "C:\Program Files\OpenSSH\OpenSSH-Win64"
-$installPath = "C:\Program Files\OpenSSH"
+$zipurl = "https://github.com/PowerShell/Win32-OpenSSH/releases/download/v9.8.1.0p1-Preview/OpenSSH-Win64.zip"
+$scriptsdir = "c:\scripts"
+$downloadpath = "$scriptsdir\OpenSSH-Win64.zip"
+$extractpath = "$scriptsdir\OpenSSH-Win64"
+$installpath = "c:\program files\OpenSSH"
 
 # Create the directory for extraction if it doesn't exist
-if (-Not (Test-Path -Path $extractPath)) {
-    New-Item -ItemType Directory -Path $extractPath
+if (-Not (Test-Path -Path $extractpath)) {
+    New-Item -ItemType Directory -Path $extractpath
 }
 
 # Create the installation directory if it doesn't exist
-if (-Not (Test-Path -Path $installPath)) {
-    New-Item -ItemType Directory -Path $installPath
+if (-Not (Test-Path -Path $installpath)) {
+    New-Item -ItemType Directory -Path $installpath
 }
 
 # Download the OpenSSH zip file
 Write-Host "Downloading OpenSSH..."
-Invoke-WebRequest -Uri $zipUrl -OutFile $downloadPath
+Invoke-WebRequest -Uri $zipurl -OutFile $downloadpath
 
 # Extract the contents of the zip file
 Write-Host "Extracting OpenSSH..."
-Expand-Archive -Path $downloadPath -DestinationPath $extractPath -Force
+Expand-Archive -Path $downloadpath -DestinationPath $extractpath -Force
 
 # Copy the extracted files to the installation directory
 Write-Host "Installing OpenSSH..."
-Copy-Item -Path "$extractPath\*" -Destination $installPath -Recurse -Force
+Copy-Item -Path "$extractpath\*" -Destination $installpath -Recurse -Force
 
 # Run the install-sshd.ps1 script (if it exists), change install to uninstall-sshd.ps1 in the below line to remove openssh
-$sshdScriptPath = "C:\Program Files\OpenSSH\OpenSSH-Win64\install-sshd.ps1"
-if (Test-Path -Path $sshdScriptPath) {
+$sshdscriptpath = "$installpath\OpenSSH-Win64\install-sshd.ps1"
+if (Test-Path -Path $sshdscriptpath) {
     Write-Host "Running install-sshd.ps1 script..."
-    powershell.exe -ExecutionPolicy Bypass -File "$sshdScriptPath"
+    powershell.exe -ExecutionPolicy Bypass -File "$sshdscriptpath"
 } else {
     Write-Host "install-sshd.ps1 script not found. Skipping SSH server installation."
 }
@@ -43,4 +44,5 @@ netsh advfirewall firewall add rule name=sshd dir=in action=allow protocol=TCP l
 Write-Host "OpenSSH Installation done"
 net start sshd
 Get-Service -Name sshd
+
 
--- wrapper-scripts/openssh/openssh-wrapper.ps1
+++ wrapper-scripts/openssh/openssh-wrapper.ps1
@@ -1,4 +1,4 @@
-$logfilepath = "C:\scripts\session.log"  # Log file inside C:\scripts folder
+$logfilepath = "c:\scripts\session.log"  # Log file inside c:\scripts folder
 
 # Start the transcript to capture everything in the PowerShell session
 Start-Transcript -Path $logfilepath -Append
@@ -6,23 +6,23 @@ Start-Transcript -Path $logfilepath -Append
 Write-Host "Starting execution of OpenSSH wrapper script..."
 
 # Define the URL of the OpenSSH script
-$opensshscripturl = "https://raw.githubusercontent.com/Wasim49/Github-Repo/refs/heads/main/actual-scripts/openssh/install-openssh.ps1"
+$openshscripturl = "https://raw.githubusercontent.com/Wasim49/Github-Repo/refs/heads/main/actual-scripts/openssh/install-openssh.ps1"
 
-# Define the local folder path where the script will be saved (C:\scripts)
-$scriptsfolder = "C:\scripts"
+# Define the local folder path where the script will be saved (c:\scripts)
+$scriptsdir = "c:\scripts"
 
-# Define the local file path where the OpenSSH script will be saved inside C:\scripts
-$installscriptpath = "$scriptsfolder\install-openssh.ps1"
+# Define the local file path where the OpenSSH script will be saved inside c:\scripts
+$installscriptpath = "$scriptsdir\install-openssh.ps1"
 
-# Create the C:\scripts folder if it doesn't exist
-if (-not (Test-Path -Path $scriptsfolder)) {
-    New-Item -Path $scriptsfolder -ItemType Directory
-    Write-Host "Created scripts folder at $scriptsfolder."
+# Create the c:\scripts folder if it doesn't exist
+if (-not (Test-Path -Path $scriptsdir)) {
+    New-Item -Path $scriptsdir -ItemType Directory
+    Write-Host "Created scripts folder at $scriptsdir."
 }
 
 # Download the OpenSSH installation script
 Write-Host "Downloading OpenSSH Installation Script..."
-Invoke-WebRequest -Uri $opensshscripturl -OutFile $installscriptpath
+Invoke-WebRequest -Uri $openshscripturl -OutFile $installscriptpath
 Write-Host "OpenSSH Installation Script downloaded to $installscriptpath."
 
 # Execute the OpenSSH installation script
@@ -33,6 +33,7 @@ Write-Host "OpenSSH Installation script executed successfully."
 
 # Stop the transcript to end capturing the session
 Stop-Transcript
+
 
 
 
