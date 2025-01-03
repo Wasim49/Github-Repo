dbcc031c04679cd5d90a7949245f894f75bd3472
Wasim49 <wasim.navas-khan@capgemini.com>
2025-01-03 18:15:02 +0000

    finalising chef workload

--- install_software/knife.rb
+++ install_software/knife.rb
@@ -4,4 +4,4 @@ log_location             STDOUT
 node_name                "wasimcg" ## This name must much the user name on chef io
 client_key               "C:/scripts/chef/install_software/.chef/wasimcg.pem"  ## you need to grab the pem file from chef io zip file
 chef_server_url          "https://api.chef.io/organizations/cheforgcg"
-cookbook_path            "C:/Users/vmadmin/Desktop/chef"    ## point this tor your local cookbook folder full path
+cookbook_path            "C:/scripts/chef/install_software"    ## point this tor your local cookbook folder full path
